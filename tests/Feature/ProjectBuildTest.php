17b4eb7a210d5f07c6fb649d274697a0d4d55435
dev-lnk <lkivanmail@gmail.com>
2025-12-08 17:53:21 +0300

    tests: pages test

--- tests/Feature/ProjectBuildTest.php
+++ tests/Feature/ProjectBuildTest.php
@@ -2,9 +2,11 @@
 
 namespace DevLnk\MoonShineBuilder\Tests\Feature;
 
+use App\MoonShine\Resources\Product\ProductResource;
 use DevLnk\MoonShineBuilder\Tests\TestCase;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
+use MoonShine\Laravel\Pages\Crud\IndexPage;
 use PHPUnit\Framework\Attributes\Test;
 
 class ProjectBuildTest extends TestCase
@@ -69,6 +71,31 @@ class ProjectBuildTest extends TestCase
             "\$table->id();",
             "\$table->string('name', 100);",
         ]);
+
+        $resourceFields = [
+            "use MoonShine\UI\Fields\ID;",
+            "use MoonShine\UI\Fields\ID;",
+            "ID::make('id')",
+            "Text::make('Name', 'name')",
+        ];
+        $indexPage = $this->resourcePath . 'Category/Pages/CategoryIndexPage.php';
+        $this->testBuildFile($indexPage, $resourceFields + [
+            "use App\MoonShine\Resources\Category\CategoryResource;",
+            "@extends IndexPage<CategoryResource>",
+        ]);
+
+        $formPage = $this->resourcePath . 'Category/Pages/CategoryFormPage.php';
+        $this->testBuildFile($formPage, $resourceFields + [
+            "use App\MoonShine\Resources\Category\CategoryResource;",
+            "@extends FormPage<CategoryResource>",
+            "'name' => ['string', 'required']",
+        ]);
+
+        $detailPage = $this->resourcePath . 'Category/Pages/CategoryDetailPage.php';
+        $this->testBuildFile($detailPage, $resourceFields + [
+            "use App\MoonShine\Resources\Category\CategoryResource;",
+            "@extends DetailPage<CategoryResource>",
+        ]);
     }
 
     /**
@@ -114,6 +141,49 @@ class ProjectBuildTest extends TestCase
             "table->timestamps();",
             "table->softDeletes();",
         ]);
+
+        $resourceFields = [
+            "use MoonShine\UI\Fields\ID;",
+            "use MoonShine\UI\Fields\Text;",
+            "use MoonShine\UI\Fields\Number;",
+            "use MoonShine\Laravel\Fields\Relationships\BelongsTo;",
+            "use MoonShine\Laravel\Fields\Relationships\HasMany;",
+            "use MoonShine\UI\Fields\Checkbox;",
+            "ID::make('id')\n\t\t\t\t->sortable()",
+            "Text::make('Name', 'title')",
+            "Text::make('Content', 'content')",
+            "Number::make('Price', 'price')\n\t\t\t\t->default(0)\n\t\t\t\t->sortable()",
+            "Number::make('Sorting', 'sort_number')",
+            "BelongsTo::make('Category', 'category', resource: CategoryResource::class)",
+            "HasMany::make('Comments', 'comments', resource: CommentResource::class)->creatable()",
+            "BelongsTo::make('User', 'moonshineUser', resource: MoonShineUserResource::class)",
+            "Checkbox::make('Active', 'is_active')",
+        ];
+
+        $indexPage = $this->resourcePath . 'Product/Pages/ProductIndexPage.php';
+        $this->testBuildFile($indexPage, $resourceFields + [
+            "use App\MoonShine\Resources\Product\ProductResource;",
+            "@extends IndexPage<ProductResource>",
+        ]);
+
+        $formPage = $this->resourcePath . 'Product/Pages/ProductFormPage.php';
+        $this->testBuildFile($formPage, $resourceFields + [
+            "use App\MoonShine\Resources\Product\ProductResource;",
+            "@extends FormPage<ProductResource>",
+            "'title' => ['string', 'required']",
+            "'content' => ['string', 'nullable']",
+            "'price' => ['int', 'required']",
+            "'sort_number' => ['int', 'required']",
+            "'category_id' => ['int', 'required']",
+            "'moonshine_user_id' => ['int', 'required']",
+            "'is_active' => ['boolean', 'required']",
+        ]);
+
+        $detailPage = $this->resourcePath . 'Product/Pages/ProductDetailPage.php';
+        $this->testBuildFile($detailPage, $resourceFields + [
+            "use App\MoonShine\Resources\Product\ProductResource;",
+            "@extends DetailPage<ProductResource>",
+        ]);
     }
 
     /**
@@ -142,6 +212,37 @@ class ProjectBuildTest extends TestCase
             "table->string('comment');",
             "table->foreignIdFor(\App\Models\Product::class, 'product_id')\n\t\t\t\t->constrained()\n\t\t\t\t->cascadeOnDelete()\n\t\t\t\t->cascadeOnUpdate()",
             "table->foreignIdFor(\\MoonShine\\Laravel\\Models\\MoonshineUser::class, 'moonshine_user_id')\n\t\t\t\t->constrained()\n\t\t\t\t->cascadeOnDelete()\n\t\t\t\t->cascadeOnUpdate()",
+        ]);
+
+        $resourceFields = [
+            "use MoonShine\UI\Fields\ID;",
+            "use MoonShine\UI\Fields\Text;",
+            "use MoonShine\Laravel\Fields\Relationships\BelongsTo;",
+            "ID::make('id')",
+            "Text::make('Comment', 'comment')",
+            "BelongsTo::make('Product', 'product', resource: ProductResource::class)",
+            "BelongsTo::make('User', 'moonshineUser', resource: MoonShineUserResource::class)",
+        ];
+
+        $indexPage = $this->resourcePath . 'Comment/Pages/CommentIndexPage.php';
+        $this->testBuildFile($indexPage, $resourceFields + [
+            "use App\MoonShine\Resources\Comment\CommentResource;",
+            "@extends IndexPage<CommentResource>",
+        ]);
+
+        $formPage = $this->resourcePath . 'Comment/Pages/CommentFormPage.php';
+        $this->testBuildFile($formPage, $resourceFields + [
+            "use App\MoonShine\Resources\Comment\CommentResource;",
+            "@extends FormPage<CommentResource>",
+            "'comment' => ['string', 'required'],",
+            "'product_id' => ['int', 'required'],",
+            "'moonshine_user_id' => ['int', 'required'],",
+        ]);
+
+        $detailPage = $this->resourcePath . 'Comment/Pages/CommentDetailPage.php';
+        $this->testBuildFile($detailPage, $resourceFields + [
+            "use App\MoonShine\Resources\Comment\CommentResource;",
+            "@extends DetailPage<CommentResource>",
         ]);
     }
 
